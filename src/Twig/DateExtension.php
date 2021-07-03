2f35212411795cf59dfcffd768f2ead927625874
albert <albert.zawadzki97@gmail.com>
2021-07-03 10:16:16 +0200

    Fixed date extension - given wrong dates

--- src/Twig/DateExtension.php
+++ src/Twig/DateExtension.php
@@ -18,12 +18,12 @@ class DateExtension extends AbstractExtension
 
     public function getStringedDate(DateTime $date): string
     {
-        $lastUpdated = $date->format('Y-m-d');
-        $today = (new DateTime())->format('Y-m-d');
-        $tomorrow = (new DateTime())->add(new DateInterval(60 * 60 * 24))->format('Y-m-d');
-        $afterTomorrow = (new DateTime())->add(new DateInterval(60 * 60 * 24 * 2))->format('Y-m-d');
-        $yesterday = (new DateTime())->sub(new DateInterval(60 * 60 * 24))->format('Y-m-d');
-        $preYesterday = (new DateTime())->sub(new DateInterval(60 * 60 * 24 * 2))->format('Y-m-d');
+        $lastUpdated = $date->format('Y.m.d');
+        $today = (new DateTime())->format('Y.m.d');
+        $tomorrow = (new DateTime())->add(new DateInterval('P1D'))->format('Y.m.d');
+        $afterTomorrow = (new DateTime())->add(new DateInterval('P2D'))->format('Y.m.d');
+        $yesterday = (new DateTime())->sub(new DateInterval('P1D'))->format('Y.m.d');
+        $preYesterday = (new DateTime())->sub(new DateInterval('P2D'))->format('Y.m.d');
 
         if ($lastUpdated === $today) {
             return 'dzisiaj';
